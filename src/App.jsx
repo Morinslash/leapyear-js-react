f611d62852af87c43bffbbb4942b663d28ab08fa
Morinslash <sebastian.elf.kulinski@gmail.com>
2025-08-06 22:16:30 +0200

    initial commit with layout setup

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+
+function App() {
+    return (
+        <div className="h-screen flex items-center justify-center">
+            <div className="w-1/2 h-1/2 bg-gray-800">
+                <div className="h-1/2 bg-gray-500">
+
+                </div>
+                <div className="w-full h-1/2 bg-gray-300 flex">
+                    <div className="w-1/2 h-full bg-blue-500">
+
+                    </div>
+                    <div className="w-1/2 h-full bg-blue-300">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default App
